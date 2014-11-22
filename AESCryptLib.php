daee41ec3eb81f31df64fb83c6a71b84efaa4c17
Philip Nicholls <phil@code67.com>
2014-11-22 15:41:00 +0100

    File Encryption Basics

--- AESCryptLib.php
+++ AESCryptLib.php
@@ -1,48 +1,44 @@
 <?php
-
 /**
  * Please see https://www.aescrypt.com/aes_file_format.html
  * for the file format used.  It should theoretically make .aes files which are
  * compatible with any AESCrypt software.
  * 
  * Sample Usage:
- * 1. Encrypt a file
- * $file_name = AESCryptLib::encryptFile('my_secret_document.docx', 'password123', 'my_encrypted_document.aes', array(
- *	array(
- *		'ident':'CREATED-BY',
- *		'data':'Philip Nicholls'
- *	)
- * ));
+ * To come later on
  * 
- * 2. Read extension blocks (AKA meta data) from a file
- * $extension_blocks = AESCryptLib::readExtensionBlocks('my_encrypted_document.aes');
- * foreach($extension_blocks as $ext_block) {
- *	$ext_ident = $ext_block['ident'];
- *	$ext_data = $ext_block['data'];
- *  print $ext_ident . "=" . $ext_data;
- * }
- * 
- * 3. Decrypt a file
- * $file_name = AESCryptLib::decryptFile('my_encrypted_document.aes', 'password123');
  */
-class AESCryptLib
+class AESCryptFile
 {
-	public static function encryptFile($source_file, $passphrase, $dest_file = NULL, $use_dynamic_filenaming = true, $ext_data = NULL)
+	const ENCRYPTED_FILE_EXTENSION = "aes";
+	
+	//http://www.leaseweblabs.com/2014/02/aes-php-mcrypt-key-padding/
+	//Only "Rijndael-128" in "Cipher-block chaining" (CBC) mode is defined as the Advanced Encryption Standard (AES).
+	private $aes_impl;
+	
+	private $use_dynamic_filenaming;
+	
+	public function __construct(AESImplementation $aes_impl, $use_dynamic_filenaming = true) {
+		$this->aes_impl = $aes_impl;
+		$this->use_dynamic_filenaming = $use_dynamic_filenaming;
+	}
+	
+	public function encryptFile($source_file, $passphrase, $dest_file = NULL, $ext_data = NULL)
 	{
 		//Check we can read the source file
-		self::checkSourceExistsAndReadable($source_file);
+		$this->checkSourceExistsAndReadable($source_file);
 		
 		//Open destination file for writing
-		$dest_fh = self::openDestinationFile($source_file, $dest_file, $use_dynamic_filenaming, true);
+		$dest_fh = $this->openDestinationFile($source_file, $dest_file, true);
 		
 		//Check any ext_data is formatted correctly
-		self::checkExtensionData($ext_data);
+		$this->checkExtensionData($ext_data);
 		
 		//Check that the password is a string (it cannot be NULL)
-		self::checkPassphraseIsValid($passphrase);
+		$this->checkPassphraseIsValid($passphrase);
 		
 		//Actually do the encryption here
-		self::doEncryptFile($source_file, $passphrase, $dest_fh, $ext_data);
+		$this->doEncryptFile($source_file, $passphrase, $dest_fh, $ext_data);
 		
 		//Return encrypted file location
 		$meta_data = stream_get_meta_data($dest_fh);
@@ -51,25 +47,26 @@ class AESCryptLib
 		return $filename;
 	}
 	
-	public static function readExtensionBlocks($source_file)
+	public function readExtensionBlocks($source_file)
 	{
 		//Check we can read the source file
-		self::checkSourceExistsAndReadable($source_file);
+		$this->checkSourceExistsAndReadable($source_file);
 		
 		//Attempt to parse and return the extension blocks only
+		//TODO
 	}
 	
-	public static function decryptFile($source_file, $passphrase, $dest_file = NULL, $use_dynamic_filenaming = true)
+	public function decryptFile($source_file, $passphrase, $dest_file = NULL)
 	{
 		//Check we can read the source file
-		self::checkSourceExistsAndReadable($source_file);
+		$this->checkSourceExistsAndReadable($source_file);
 		
 		//Open destination file for writing
-		$dest_fh = self::openDestinationFile($source_file, $dest_file, $use_dynamic_filenaming, false);
+		$dest_fh = $this->openDestinationFile($source_file, $dest_file, false);
 		
 		//Check whether the passphrase is correct before decrypting the keys and validating with HMAC1
 		//If it is, attempt to decrypt the file using these keys and write to destination file
-		self::doDecryptFile($source_file, $passphrase, $dest_fh);
+		$this->doDecryptFile($source_file, $passphrase, $dest_fh);
 		
 		//Return encrypted file location
 		$meta_data = stream_get_meta_data($dest_fh);
@@ -78,7 +75,7 @@ class AESCryptLib
 		return $filename;
 	}
 	
-	private static function checkSourceExistsAndReadable($source_file)
+	private function checkSourceExistsAndReadable($source_file)
 	{
 		//Source file must exist
 		if (!file_exists($source_file)) {
@@ -91,30 +88,31 @@ class AESCryptLib
 		}
 	}
 	
-	private static function openDestinationFile($source_file, $dest_file, $use_dynamic_filenaming, $encrypting = true) {
+	private function openDestinationFile($source_file, $dest_file, $encrypting = true) {
 		
 		//Please use checkSourceExistsAndReadable on the source before running this function as we assume it exists here
 		$source_info = pathinfo($source_file);
 		
 		if (is_null($dest_file)) {
 			if (!$encrypting) {
-				//We are decrypting, attempt to lookup original file name from encrypted source file
-				$orig_file_name = self::lookupOriginalFileNameFromSourceMetaData($source_file);
-				if (!is_null($orig_file_name)) {
-					//This will only be the filename and extension, not the path
-					$dest_file = $source_info['dirname'] . DIRECTORY_SEPARATOR . $orig_file_name;
+				//We are decrypting without a known destination file
+				//We should check for a double extension in the file name e.g. (filename.docx.aes)
+				//Actually, we just check it ends with .aes and strip off the rest
+				if (preg_match("/^(.+)\." . AESCryptFile::ENCRYPTED_FILE_EXTENSION . "$/i", $source_info['basename'], $matches)) {
+					//Yes, source is an .aes file
+					//We remove the .aes part and use a destination file in the same source directory
+					$dest_file = $source_info['dirname'] . DIRECTORY_SEPARATOR . $matches[1];
 				} else {
-					//Not found, we cannot continue because we have no idea what the file extension should be
-					//User should specify dest file path explicitly
-					throw new AESCryptUnknownOriginalFileExtensionException($source_file);
+					throw new AESCryptCannotInferDestinationException($source_file);
 				}
+				
 			} else {
 				//We are encrypting, use .aes as destination file extension
-				$dest_file = $source_info['dirname'] . DIRECTORY_SEPARATOR . $source_info['filename'] . ".aes";
+				$dest_file = $source_info['dirname'] . DIRECTORY_SEPARATOR . $source_info['filename'] .  "." . AESCryptFile::ENCRYPTED_FILE_EXTENSION;
 			}
 		}
 		
-		if ($use_dynamic_filenaming) {
+		if ($this->use_dynamic_filenaming) {
 			//Try others until it doesnt exist
 			$dest_info = pathinfo($dest_file);
 			
@@ -131,12 +129,15 @@ class AESCryptLib
 		}
 		
 		//Now that we found a non existing file, attempt to open it for writing
-		$dest_fh = fopen($dest_file, "w");
+		$dest_fh = fopen($dest_file, "x");
+		if ($dest_fh === false) {
+			throw new AESCryptFileAccessException("Cannot create for writing:" . $dest_file);
+		}
 				
 		return $dest_fh;
 	}
 	
-	private static function checkExtensionData($ext_data)
+	private function checkExtensionData($ext_data)
 	{
 		if (is_null($ext_data)) {
 			return;
@@ -159,12 +160,74 @@ class AESCryptLib
 		}
 	}
 	
-	private static function checkPassphraseIsValid($passphrase)
+	private function checkPassphraseIsValid($passphrase)
 	{
 		if (is_null($passphrase)) {
 			throw new AESCryptInvalidPassphraseException("NULL passphrase not allowed");
 		}
 	}
+	
+	private function doEncryptFile($source_file, $passphrase, $dest_fh, $ext_data)
+	{
+		//Create a random IV using the aes implementation
+		//IV is based on the block size which is 128 bits (16 bytes) for AES
+		$iv_1 = $this->aes_impl->create_iv();
+		if (strlen($iv_1) != 16) {
+			throw new AESCryptImplementationException("Returned an IV which is not 16 bytes long: " . bin2hex($iv_1));
+		}
+		//Use this IV and password to generate the first encryption key
+		//We dont need to use AES for this as its just lots of sha hashing
+		$enc_key_1 = $this->createKeyUsingIVAndPassphrase($iv_1, $passphrase);
+		if (strlen($enc_key_1) != 32) {
+			throw new Exception("Returned a passphrase which is not 32 bytes long: " . bin2hex($enc_key_1));
+		}
+
+		//Create another set of keys to do the actual file encryption
+		$iv_2 = $this->aes_impl->create_iv();
+		if (strlen($iv_2) != 16) {
+			throw new AESCryptImplementationException("Returned an IV which is not 16 bytes long: " . bin2hex($iv_2));
+		}
+		//The file format uses AES 256 (which is the key length)
+		$enc_key_2 = $this->aes_impl->createRandomKey();
+		if (strlen($enc_key_2) != 32) {
+			throw new AESCryptImplementationException("Returned a random key which is not 32 bytes long: " . bin2hex($enc_key_2));
+		}
+		
+		//Encrypt the second set of keys using the first keys
+		$file_encryption_keys = $iv_2 . $enc_key_2;
+		$encrypted_keys = $this->aes_impl->encryptData($file_encryption_keys, $iv_1, $enc_key_1);
+		$this->assertLength($encrypted_keys, 48);
+		
+		//Calculate HMAC1 using the first enc key
+		$hmac_1 = hash_hmac("sha256", $file_encryption_keys, $enc_key_1, true);
+		$this->assertLength($hmac_1, 32);
+		
+		//Now do file encryption
+		$source_contents = file_get_contents($source_file);
+		$encrypted_file_data = $this->aes_impl->encryptData($source_contents, $iv_2, $enc_key_2);
+		
+		$file_size_modulo = str_len($source_contents)%16;
+		
+		//HMAC the encrypted data too
+		$hmac_2 = hash_hmac("sha256", $encrypted_file_data, $enc_key_2, true);
+		
+		//TODO actaully write it to the dest fh
+	}
+	
+	//This is sha256 by standard and should always returns 256bits (32 bytes) of hash data
+	//Looking at the java implementation, it seems we should iterate the hasing 8192 times
+	private function createKeyUsingIVAndPassphrase($iv, $passphrase) 
+	{
+		$aes_key = $iv;
+		$iterations = 8192;
+		for($i=0; $i<$iterations; $i++)
+		{
+			$aes_key = hash("sha256", $aes_key . $passphrase, true);
+		}
+		return $aes_key;
+	}
+	
+	
 }
 
 class AESCryptMissingDependencyException extends Exception {} //E.g. missing mcrypt
@@ -175,4 +238,5 @@ class AESCryptFileAccessException extends Exception {} //E.g. read/write error o
 class AESCryptFileExistsException extends Exception {} //E.g. when a destination file exists (we never overwrite)
 class AESCryptInvalidExtensionException extends Exception {} //E.g. when an extension array is invalid
 class AESCryptInvalidPassphraseException extends Exception {} //E.g. when an extension array is invalid
-class AESCryptUnknownOriginalFileExtensionException extends Exception {} //E.g. when we try to decrypt a 3rd party written file which doesnt contain the meta data extension which tells us the ORIGINAL-FILE-NAME
+class AESCryptCannotInferDestinationException extends Exception {} //E.g. when we try to decrypt a 3rd party written file which doesnt have the standard file name convention
+class AESCryptImplementationException extends Exception {} //For generic exceptions by the aes implementation used
